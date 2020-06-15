78fba4b511d867c24d229296b7328de2c3978bff
MMendes24 <mmbusiness1@protonmail.com>
2020-06-15 16:06:15 -0400

    Second stretch goal researched. I did a lot of digging and between the explanation of the TLs and my own study I think I mostly grab the mechanics of closure and how we use it to invoke inner functions.

--- stretch.js
+++ stretch.js
@@ -19,3 +19,24 @@ console.log("b defined? " + (typeof b !== 'undefined'));
 /* I assumed it looked like var b = 3 and var a = b, but what js actually reads is b = 3, var a = b. Console logging them inside the function makes them both 
 defined. That was honestly pretty informative. */
 
+// /Write a function that would allow you to do this using a closure. 
+// (This is another interview question we've seen before - when you're ready for answers, view an explanation here).
+
+function createBase(baseNumber) {
+    return function(x) {
+      return baseNumber + x;
+    }
+  }
+  
+  let addSix = createBase(6);
+  
+  addSix(12)
+
+  //it took me some time to grasp, but addSix in this case is a variable that equals the function with an argument already passed to it. Because it already has this
+  //first argument, it essentially operates as a shorthand for that function but with the first parameter already entered. This is why when you call addSix the argument
+  //that you enter in the parantheses doesn't replace baseNumber (because that's already been accounted for) but rather functions as x, as if you had called
+  //createBase(6)(12) to begin with. Effectively, the first part of the function (the outer) has already been invoked and it just needs the remainder (the inner). The
+  //big difference in this case is that addSix allows us to call the variable with that parameter already known as an argument identically every time, rather than
+  //always having to enter six. Closure is the reason this works, because return baseNumber + x; is able to perform even though "return function(x)" has terminated. 
+
+  
